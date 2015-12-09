fa176ca13e0c90c725b096a20382f2b3d0343618
LeGuit <guillaume.woodward@gmail.com>
2015-12-09 10:08:41 +0100

    Update read_format.c

--- read_format.c
+++ read_format.c
@@ -18,25 +18,22 @@ int				read_format(const char *format, va_list arg)
 	size_t		i;
 
 	cursor = format;
-	mod.charprint = 0;
 	i = 0;
 	while (cursor[i])
 	{
-		if (cursor[i] == '%' && cursor[i + 1] == '%') // thinks about counting 
+		if (cursor[i] == '%') // thinks about counting 
 		{
-			cursor[i + 1] = 0;
+			if (cursor[i + 1] == '%')
+				cursor[i + 1] = 0;
+			else
+			{
+				mod.charprint = get_mod(i - 1, cursor[i + 1], &mod, arg)
+				cursor[i] = 0;
+			}
 			ft_putstr(cursor);
-			cursor += i + 1;
+			if ((cursor[i] == '%') ? cursor += i : cursor += i + 1);
 		}
-		if (cursor[i] == '%' && cursor [i + 1] == '%')//get a mod
-		{
-			cursor[i] = 0;
-			ft_putstr(cursor);
-			i += get_mod(i - 1, cursor[i + 1], &mod, arg); 	// return i on end of mod + 1 (i - 1 for nb_char cause whe don't count the %)
-			cursor += i;									// care of exact size of mod
-			i = -1;
-		}
-		i++;
+		i++;// check if good position
 	}
 	i = mod.charprint;
 	return (i);
